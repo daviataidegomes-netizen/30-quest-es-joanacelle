998d6b6ea52a5370126df484497e0d8f17a11256
daviataidegomes-netizen <daviataidegomes@gmail.com>
2026-04-06 08:17:04 -0300

    Update 3 (1).py

--- 3 (1).py
+++ 3 (1).py
@@ -1,4 +1,4 @@
-# tarefa 3=
+# tarefa 3= Leia um valor e: Mostre o valor digitado; Mostre o tipo da variável usando type(
 valor = ('30 questões são loucura')
 variavel = type(valor)
 print(variavel)
