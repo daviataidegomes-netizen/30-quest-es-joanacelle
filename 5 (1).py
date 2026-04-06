6f9f7ca907c99a4bc63cbbde57283f74268f9190
daviataidegomes-netizen <daviataidegomes@gmail.com>
2026-04-06 08:18:03 -0300

    Update 5 (1).py

--- 5 (1).py
+++ 5 (1).py
@@ -1,4 +1,4 @@
-# tarefa 5=
+# tarefa 5= Leia um número como texto (input) e: Converta para inteiro; Mostre o triplo
 n1 = int(input('digite o valo de um numero inteiro: '))
 triplo = 3 * n1
 print(n1)
