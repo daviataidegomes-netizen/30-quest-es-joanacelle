428dc4c637f27ebd3a7f48fdae408b292a059465
daviataidegomes-netizen <daviataidegomes@gmail.com>
2026-04-06 08:16:36 -0300

    Update 2 (1).py

--- 2 (1).py
+++ 2 (1).py
@@ -1,4 +1,4 @@
-# tarefa 2=
+# tarefa 2= Leia um número (entrada padrão do input) e: Converta para inteiro; Exiba o dobro do valor
 numero = int(input('digite um valor inteiro: '))
 dobro = 2 * numero
 print(dobro)
